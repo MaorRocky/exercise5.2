2dc49188d55e2f810d62fc891e10dd983e7a62b3
Maor <maor@365scores.com>
2021-01-07 13:11:11 +0200

    less

--- exercise5.2/ViewController.swift
+++ exercise5.2/ViewController.swift
@@ -37,46 +37,21 @@ class ViewController: UIViewController
         myView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
 
 
-        let label = VerticalTopAlignLabel(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
         myView.addSubview(label)
         label.text = "aaaa"
         label.textColor = .black
         label.numberOfLines = 0
 
-
+        
         label.translatesAutoresizingMaskIntoConstraints = false
         label.leadingAnchor.constraint(equalTo: myView.leadingAnchor, constant: 8).isActive = true
         label.topAnchor.constraint(equalTo: myView.topAnchor, constant: 8).isActive = true
         label.trailingAnchor.constraint(equalTo: myView.trailingAnchor, constant: -8).isActive = true
-        label.bottomAnchor.constraint(equalTo: myView.bottomAnchor, constant: -8).isActive = true
+        label.bottomAnchor.constraint(lessThanOrEqualTo: myView.bottomAnchor, constant: -8).isActive = true
 
 
     }
 
 
 }
-
-class VerticalTopAlignLabel: UILabel
-{
-
-    override func drawText(in rect: CGRect)
-    {
-        guard let labelText = text else
-        {
-            return super.drawText(in: rect)
-        }
-
-        let attributedText = NSAttributedString(string: labelText, attributes: [NSAttributedString.Key.font: font ?? "error"])
-        var newRect = rect
-        newRect.size.height = attributedText.boundingRect(with: rect.size, options: .usesLineFragmentOrigin, context: nil).size.height
-
-        if numberOfLines != 0
-        {
-            newRect.size.height = min(newRect.size.height, CGFloat(numberOfLines) * font.lineHeight)
-        }
-
-        super.drawText(in: newRect)
-    }
-
-}
-
