6558f1cb007da63cab2a68414cc84f01e31a573c
Maor <maor@365scores.com>
2021-01-07 12:29:14 +0200

    דד

--- exercise5.2/ViewController.swift
+++ exercise5.2/ViewController.swift
@@ -15,8 +15,6 @@
 //The label should have minimum padding of 8pt from all view’s edges
 
 
-
-
 import UIKit
 
 class ViewController: UIViewController
@@ -28,8 +26,8 @@ class ViewController: UIViewController
         let myView = UIView()
         myView.backgroundColor = .red
         self.view.addSubview(myView)
-       
-     
+
+
         myView.translatesAutoresizingMaskIntoConstraints = false
         myView.widthAnchor.constraint(lessThanOrEqualToConstant: 300).isActive = true
         myView.heightAnchor.constraint(lessThanOrEqualToConstant: 200).isActive = true
@@ -39,21 +37,18 @@ class ViewController: UIViewController
         myView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
 
 
-
         let label = VerticalTopAlignLabel(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
         myView.addSubview(label)
         label.text = "aaaa"
         label.textColor = .black
         label.numberOfLines = 0
 
-    
-        
+
         label.translatesAutoresizingMaskIntoConstraints = false
         label.leadingAnchor.constraint(equalTo: myView.leadingAnchor, constant: 8).isActive = true
         label.topAnchor.constraint(equalTo: myView.topAnchor, constant: 8).isActive = true
         label.trailingAnchor.constraint(equalTo: myView.trailingAnchor, constant: -8).isActive = true
         label.bottomAnchor.constraint(equalTo: myView.bottomAnchor, constant: -8).isActive = true
-        
 
 
     }
@@ -61,16 +56,22 @@ class ViewController: UIViewController
 
 }
 
-class VerticalTopAlignLabel: UILabel {
+class VerticalTopAlignLabel: UILabel
+{
 
-    override func drawText(in rect:CGRect) {
-        guard let labelText = text else {  return super.drawText(in: rect) }
+    override func drawText(in rect: CGRect)
+    {
+        guard let labelText = text else
+        {
+            return super.drawText(in: rect)
+        }
 
         let attributedText = NSAttributedString(string: labelText, attributes: [NSAttributedString.Key.font: font ?? "error"])
         var newRect = rect
         newRect.size.height = attributedText.boundingRect(with: rect.size, options: .usesLineFragmentOrigin, context: nil).size.height
 
-        if numberOfLines != 0 {
+        if numberOfLines != 0
+        {
             newRect.size.height = min(newRect.size.height, CGFloat(numberOfLines) * font.lineHeight)
         }
 
